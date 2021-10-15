e2404abf458f272d6d850f63da8b2db89d1624c3
nathan <nathan.titirangi@gmail.com>
2021-10-16 12:10:35 +1300

    Added a model registerer

--- src/main/java/naenaenz/biomes/plus/BiomesPlusMain.java
+++ src/main/java/naenaenz/biomes/plus/BiomesPlusMain.java
@@ -2,13 +2,15 @@ package naenaenz.biomes.plus;
 
 import naenaenz.biomes.plus.registry.RegisterEntities;
 import naenaenz.biomes.plus.registry.RegisterItems;
+import naenaenz.biomes.plus.registry.RegisterModels;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import software.bernie.geckolib3.GeckoLib;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class BiomesPlusMain implements ModInitializer {
+public class BiomesPlusMain implements ModInitializer, ClientModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -25,5 +27,10 @@ public class BiomesPlusMain implements ModInitializer {
 		GeckoLib.initialize();
 		new RegisterItems().Register();
 		new RegisterEntities().Register();
+	}
+
+	@Override
+	public void onInitializeClient() {
+		new RegisterModels().Register();
 	}
 }
--- src/main/java/naenaenz/biomes/plus/registry/RegisterModels.java
+++ src/main/java/naenaenz/biomes/plus/registry/RegisterModels.java
@@ -1,15 +1,13 @@
-package naenaenz.biomes.plus;
+package naenaenz.biomes.plus.registry;
 
 import naenaenz.biomes.plus.client.renderers.GargrunkelGeoRenderer;
 import naenaenz.biomes.plus.registry.EntityRegistry;
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 
-public class ClientListener implements ClientModInitializer {
+public class RegisterModels {
 
-    @Override
-    public void onInitializeClient() {
+    public void Register() {
         EntityRendererRegistry.INSTANCE.register(EntityRegistry.GARGRUNKEL_ENTITY,
                 (context) -> new GargrunkelGeoRenderer(context));
         ClientSidePacketRegistry.INSTANCE.register(software.bernie.example.ClientListener.EntityPacket.ID, (ctx, buf) -> {
